c64b8798877277826976b5d48deaf57b5124b983
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-07-03 23:01:00 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         Pizza basePizza = new Pizza(false);
-        basePizza.getPizzaPrice();
+        basePizza.addExtraCheese();
+        basePizza.addExtraToppings();
+        basePizza.takeaway();
+        basePizza.getBill();
 
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,19 +1,39 @@
 public class Pizza {
-    private int prize;
+    private int price;
     private boolean veg;
+
+    private int extraCheesePrice = 100;
+    private int extraToppingsPrice = 150;
+    private int backpackPrice = 20;
 
     public Pizza(boolean veg) {
         this.veg = veg;
         if (this.veg) {
-            this.prize = 300;
+            this.price = 300;
         } else {
-            this.prize = 400;
+            this.price = 400;
         }
     }
 
+    public void addExtraCheese(){
+        System.out.println("Extra cheese added");
+        this.price += extraCheesePrice;
 
-    public void getPizzaPrice() {
+    }
 
-        System.out.println(this.prize);
+    public void addExtraToppings(){
+        System.out.println("Extra toppings added");
+        this.price += extraToppingsPrice;
+    }
+
+    public void takeaway(){
+        System.out.println("Take away opted");
+        this.price += backpackPrice;
+
+    }
+
+    public void getBill(){
+        System.out.println(this.price);
+
     }
 }
